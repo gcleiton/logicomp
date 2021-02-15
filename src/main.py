3feba66e28b8c0f892ce4ed782a618fbff23f430
Gabriel Cleiton <ccgabrielcc2609@gmail.com>
2021-02-15 11:47:53 -0300

    feat: ensure number_of_atoms show for each formula the quantity of atoms

--- src/main.py
+++ src/main.py
@@ -14,37 +14,60 @@ formula7 = Implies(Not(And(Atom('p'), Atom('s'))), And(Atom('q'), Atom('r')))  #
 formula8 = Implies(Not(And(Atom('p'), Atom('s'))), And(Atom('q'), Not(And(Atom('p'), Atom('s')))))
 # ((¬(p /\ s)) -> (q /\ (¬(p /\ s))))
 
+def main():
+    check_quantity_of_atoms()
 
-print(formula1 == formula3)
-print(formula1 == formula2)
-print(formula3 == And(Atom('p'), Atom('q')))
+def check_quantity_of_atoms ():
+    for formula in get_formulas():
+        print(f'Formula: { formula }')
+        print(f'Quantity of atoms: { number_of_atoms(formula) }')
+        print('--------------------------------------------')
 
-print('formula1:', formula1)
-print('formula2:', formula2)
-print('formula3:', formula3)
-print('formula4:', formula4)
-print('formula5:', formula5)
-print('formula6:', formula6)
-print('formula7:', formula7)
-print('formula8:', formula8)
+def get_formulas ():
+    return [
+        formula1, formula2, formula3, formula4, 
+        formula5, formula6, formula7, formula8
+    ]
 
-print('length of formula1:', length(formula1))
-print('length of formula3:', length(formula3))
+def show_formulas ():
+    formulas = get_formulas()
+    for i, formula in enumerate(formulas):
+        print(f'formula{i+1}: {formula}')
 
-print('length of formula7:', length(formula7))
 
-print('subformulas of formula7:')
-print(subformulas(formula7))
-for subformula in subformulas(formula7):
-    print(subformula)
+    
+    # print(formula1 == formula3)
+    # print(formula1 == formula2)
+    # print(formula3 == And(Atom('p'), Atom('q')))
 
-print('length of formula8:', length(formula8))
-print('subformulas of formula8:')
-for subformula in subformulas(formula8):
-    print(subformula)
+    # print('formula1:', formula1)
+    # print('formula2:', formula2)
+    # print('formula3:', formula3)
+    # print('formula4:', formula4)
+    # print('formula5:', formula5)
+    # print('formula6:', formula6)
+    # print('formula7:', formula7)
+    # print('formula8:', formula8)
 
-#  we have shown in class that for all formula A, len(subformulas(A)) <= length(A):
-# for example, for formula8:
-print('number of subformulas of formula8:', len(subformulas(formula8)))
-print('len(subformulas(formula8)) <= length(formula8):', len(subformulas(formula8)) <= length(formula8))
+    # print('length of formula1:', length(formula1))
+    # print('length of formula3:', length(formula3))
 
+    # print('length of formula7:', length(formula7))
+
+    # print('subformulas of formula7:')
+    # print(subformulas(formula7))
+    # for subformula in subformulas(formula7):
+    #     print(subformula)
+
+    # print('length of formula8:', length(formula8))
+    # print('subformulas of formula8:')
+    # for subformula in subformulas(formula8):
+    #     print(subformula)
+
+    # #  we have shown in class that for all formula A, len(subformulas(A)) <= length(A):
+    # # for example, for formula8:
+    # print('number of subformulas of formula8:', len(subformulas(formula8)))
+    # print('len(subformulas(formula8)) <= length(formula8):', len(subformulas(formula8)) <= length(formula8))
+
+if __name__ == "__main__":
+  main()
--- src/utils/functions.py
+++ src/utils/functions.py
@@ -52,8 +52,12 @@ def atoms(formula):
     This piece of code above prints: p, s
     (Note that there is no repetition of p)
     """
-    pass
-    # ======== YOUR CODE HERE ========
+    if isinstance(formula, Atom):
+        return {formula}
+    if isinstance(formula, Not):
+        return atoms(formula.inner)
+    if isinstance(formula, And) or isinstance(formula, Or) or isinstance(formula, Implies):
+        return atoms(formula.right).union(atoms(formula.left))
 
 
 def number_of_atoms(formula):
@@ -63,8 +67,12 @@ def number_of_atoms(formula):
 
     must return 3 (Observe that this function counts the repetitions of atoms)
     """
-    pass
-    # ======== YOUR CODE HERE ========
+    if isinstance(formula, Atom):
+        return 1
+    if isinstance(formula, Not):
+        return number_of_atoms(formula.inner)
+    if isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
+        return number_of_atoms(formula.left) + number_of_atoms(formula.right)
 
 
 def number_of_connectives(formula):
